6d593370de21bb1f6b060c5953a166e3f878b8da
Matthías Leó G. Turpeinen <matthias.turpeinen@aico.ai>
2025-04-14 19:33:25 +0300

    WIP Kendo 10 TwBS theme overwrites

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Button } from "@progress/kendo-vue-buttons";
+
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="card">
+    <button type="button">Regular button</button>
+    <button class="btn btn-primary">Bootstrap Button</button>
+    <Button themeColor="primary">Kendo Button</Button>
+  </div>
+</template>
+
+<style scoped>
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px; /* Adds spacing between buttons */
+    height: fit-content;
+    padding: 25px;
+  }
+</style>
+
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './assets/global.scss';
+import { createApp } from "vue";
+import App from "./App.vue";
+
+
+const app = createApp(App);
+app.mount("#app");
